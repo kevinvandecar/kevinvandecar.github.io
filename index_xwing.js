c9c6c466c41d89d095ce48a3489928e9bdc93893
gonzomustang <kevin.vandecar@autodesk.com>
2020-02-12 22:38:06 -0500

    xwing example

---
+++ index_xwing.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// global viewer
+
+let viewer = null;
+
+// setup a new model in the viewer
+function viewit(modelName, lightPreset) {
+
+	
+     var options = {
+        'document' : modelName, 
+         'env':'Local', 
+        };
+    var viewerElement = document.getElementById('viewer3D');
+
+    viewer = new Autodesk.Viewing.Private.GuiViewer3D (viewerElement, {}); //Autodesk.Viewing.Viewer3D(viewerElement, {});
+
+    Autodesk.Viewing.Initializer(options,function() {
+        viewer.initialize();
+		viewer.addEventListener (Autodesk.Viewing.GEOMETRY_LOADED_EVENT, function (event) {
+                    setTimeout (function () { orient_view(); }, 100) ;
+                }) ;		
+        viewer.load(options.document);
+        viewer.setLightPreset(lightPreset);
+    });
+ 
+} 
+
+// tell the viewer to fit the geometry to the view extents.
+function orient_view () {
+      if (viewer != null) {
+		 var front = new THREE.Vector3(55, -75, 88);
+		 viewer.navigation.setPosition(front);
+		 viewer.fitToView (true) ;
+		 viewer.createViewCube();
+		 viewer.displayViewCube(true);
+      } else{
+			console.log("viewer is null");
+	  }
+}
+
+viewit( "https://kevinvandecar.github.io/assets/x-wing_max/svf/xwing.SVF", 7);
+
+
+
